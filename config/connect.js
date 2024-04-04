df787043760e5e12d2c8b5198163d0fbe8c013f4
Lucas Caceres <lucas.caceres.lc@hotmai.com>
2024-04-04 01:02:10 -0300

    connect db api

--- config/connect.js
+++ config/connect.js
@@ -3,13 +3,17 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const sequelize = new Sequelize({
+const config =
+  "postgres://case_router_user:l4gFkroZtimwFtsFM55pKOFq4Rq61gnP@dpg-co71tfen7f5s738f9rt0-a/case_router";
+const configsql = {
   host: process.env.HOST_DB,
   username: process.env.USER_DB,
   password: process.env.PASSWORD_DB,
   database: process.env.DATABASE,
   port: process.env.PORT_DB,
   dialect: "mysql",
-});
+};
+
+const sequelize = new Sequelize(config);
 
 module.exports = sequelize;
