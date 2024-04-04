4e2fab8b6f1a838f159b13e816751ad3583fd4f3
Lucas Caceres <lucas.caceres.lc@hotmai.com>
2024-04-04 02:00:06 -0300

    add cors

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ app.use(express.json({ limit: "50mb" }));
 
 app.use(express.static("public"));
 
-app.use(
-  cors({
-    origin: "https://case-router-front.vercel.app/",
-  })
-);
+app.use(cors());
 
 sequelize
   .sync()
